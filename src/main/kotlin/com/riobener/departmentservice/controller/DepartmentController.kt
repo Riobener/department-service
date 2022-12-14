11bfdfaabf9d315ab84b2f1cdbf863f9f4ab3c51
Riobener <v.shevchenko@energochain.ru>
2022-12-15 01:48:00 +0300

    initial commit

---
+++ src/main/kotlin/com/riobener/departmentservice/controller/DepartmentController.kt
@@ -0,0 +1,38 @@
+package com.riobener.departmentservice.controller
+
+import com.riobener.departmentservice.entity.Department
+import com.riobener.departmentservice.service.DepartmentService
+import io.opentracing.Span
+import io.opentracing.SpanContext
+import io.jaegertracing.internal.JaegerSpanContext
+import io.opentracing.Tracer
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+
+@RestController
+@RequestMapping("/departments")
+class DepartmentController(
+    private val departmentService: DepartmentService,
+) {
+
+    @GetMapping("/health")
+    fun checkHealth() = "All good"
+
+    @PostMapping("/")
+    fun saveDepartment(@RequestParam name: String, address: String, code: String): Department {
+        return departmentService.saveDepartment(
+            Department(
+                id = UUID.randomUUID(),
+                departmentName = name,
+                departmentAddress = address,
+                departmentCode = code
+            )
+        )
+    }
+
+    @GetMapping("/{id}")
+    fun findDepartmentById(@PathVariable("id") departmentId: String): Department? {
+        return departmentService.findDepartmentById(UUID.fromString(departmentId))
+    }
+}
